35b6e3fdad4126eeddf947e4e7f49b3c3889ab63
Thomas Herrmann <mail@thoherr.de>
2017-08-05 15:30:30 +0200

    use promises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -70,13 +70,17 @@ export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
+
   constructor(private heroService: HeroService) { }
+
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+
   ngOnInit(): void {
     this.getHeroes();
   }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
